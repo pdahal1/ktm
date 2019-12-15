09ee54f21ea49fdae36e3249320778825f83fbce
pdahal1 <dahalpar@gmail.com>
2019-12-15 01:02:17 -0500

    adding zip

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 const express = require('express'); 
 const path = require("path"); 
 var app = express(); 
-app.listen(8081, () => console.log('app fired on port 8081'));
+app.listen(8081, () => console.log('app fired on ports 8081'));
 app.use('/', express.static(path.join(__dirname, './ktm'))); 
