8b8919cf76778d29e150029c2e41835d2452ee8f
Parang <pdahal1@users.noreply.github.com>
2019-12-15 22:09:08 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,4 @@ const express = require('express');
 const path = require("path"); 
 var app = express(); 
 app.listen(8081, () => console.log('app fired on ports 8081'));
-app.use('/', express.static(path.join(__dirname, './ktm'))); 
+app.use('/', express.static(path.join(__dirname, 'ktm'))); 
